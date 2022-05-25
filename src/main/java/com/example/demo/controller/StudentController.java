b7db029fed62b09965a35220dedf197433e54864
username-hello <nikitos.burak@gmail.com>
2022-05-25 11:21:27 +0300

    renaming packages

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,6 @@
-package com.example.demo.student;
+package com.example.demo.controller;
 
+import com.example.demo.domain.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
